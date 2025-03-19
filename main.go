00c98996eee45054a79043c0c7e2aba209156670
Steven Stringer <sstringer@gentlemanfox.co.uk>
2025-03-19 17:56:00 +0000

    fixed output

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 
 func main() {
 
+	m1 := make(map[string]string)
+	m2 := make(map[string]string)
+	m1["msg1"] = "The first message"
+	m1["msg2"] = "The second message"
+	m1["msg3"] = "You get the idea..."
+
 	var ctx = context.Background()
 
 	rdb := redis.NewClient(&redis.Options{
@@ -18,18 +24,26 @@ func main() {
 	})
 
 	/*Put some stuff in Redis*/
-	err := rdb.Set(ctx, "m1", "This is the primary message", 0).Err()
-	if err != nil {
-		slog.Error("redis error", "error string", err.Error())
-		return
+	for k, v := range m1 {
+		err := rdb.Set(ctx, k, v, 0).Err()
+		if err != nil {
+			slog.Error("redis error", "error string", err.Error())
+			return
+		}
 	}
 
-	val, err := rdb.Get(ctx, "m1").Result()
-	if err != nil {
-		slog.Error("redis error", "error string", err.Error())
-		return
+	for k, _ := range m1 {
+
+		val, err := rdb.Get(ctx, k).Result()
+		if err != nil {
+			slog.Error("redis error", "error string", err.Error())
+			return
+		}
+		m2[k] = val
 	}
 
-	slog.Info("redis get", "key", val)
-	return
+	/* output will be unsorted */
+	for k, v := range m2 {
+		slog.Info("redis get", "key", k, "value", v)
+	}
 }
